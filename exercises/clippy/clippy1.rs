7906109187a5f442314f545cb7af157c555ac48b
lixiang <1395734327@qq.com>
2025-12-04 09:38:56 +0800

    fix clippy

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -10,14 +10,13 @@
 // hint.
 
 
-
 use std::f32;
 
 fn main() {
-    let pi = f32::consts::PI;
+    const PI: f32 = f32::consts::PI;
     let radius = 5.00f32;
 
-    let area = pi * f32::powi(radius, 2);
+    let area = PI * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,21 +9,21 @@
 fn main() {
     let my_option: Option<()> = None;
     if let Some(x) = my_option {
-        // my_option.unwrap();
     }
 
     let my_arr = &[
         -1, -2, -3,
-        -4, -5, -6,
+        -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec:std::vec::Vec<i32> = vec![];
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.clear();
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    std::mem::swap(&mut value_a,&mut value_b);
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
